bdbaa4603288ecafac73934e716b2064774d5f4a
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-06-15 13:12:52 +0200

    Added request builder class.

---
+++ src/test/java/RequestBuilder/NdosiApiRequestBuilder.java
@@ -0,0 +1,31 @@
+package RequestBuilder;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static Common.BasePaths.*;
+import static PayloadBuilder.NdosiApiPayloadBuilder.*;
+
+public class NdosiApiRequestBuilder {
+
+    public static String loginPath = "login";
+    public static String userToken;
+
+
+    public static Response loginUser() {
+        Response response = RestAssured.given()
+                .baseUri(ndosiApiBaseUrl)
+                .basePath(loginPath)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(loginPayload().toJSONString())
+                .post()
+                .then()
+                .extract().response();
+
+        userToken = response.jsonPath().getString("data.token");
+
+        return response;
+    }
+}
